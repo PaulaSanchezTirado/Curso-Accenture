ce82c5736e2b06d0b1c572d88239aefe3bfbfafe
Paula <paula.sanchez.tirado@accenture.com>
2023-01-18 11:06:04 +0100

    Ejercicio 4. Apartado C (día 18)

---
+++ CursoAccenture/src/Ejercicio4Eclipse/Ejercicio4ApartadoC.java
@@ -0,0 +1,22 @@
+package Ejercicio4Eclipse;
+
+public class Ejercicio4ApartadoC {
+
+	final static double PI = Math.PI;
+	final static int RADIO = 23;
+	
+	public static void main(String[] args) {
+			
+		double areaCirculo;
+		double longitudCircunferencia;
+		
+		areaCirculo = PI*RADIO*RADIO;
+		longitudCircunferencia = 2*PI*RADIO;
+		
+		System.out.println("El área del círculo es: " + areaCirculo );
+		System.out.println("La longitud de la circunferencia es " + longitudCircunferencia);
+		
+
+	}
+
+}
