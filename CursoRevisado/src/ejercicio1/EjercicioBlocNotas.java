ab93ff7903106251ada0335a989058e78ef62a17
Paula <paula.sanchez.tirado@accenture.com>
2023-01-19 22:24:36 +0100

    Ejercicio del bloc de notas ya ejecutado en la consola

---
+++ CursoRevisado/src/ejercicio1/EjercicioBlocNotas.java
@@ -0,0 +1,28 @@
+package ejercicio1;
+
+/**
+ * Ejercicio 1 de la presentación Programación Java
+ * @author paula.sanchez.tirado
+ *
+ */
+
+public class EjercicioBlocNotas {
+	
+	/**
+	 * Imprime por pantalla varios números 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		int a = 11;
+		int b = 27;
+		int c = 37;
+		int d = 47;
+		
+		// Se utilizan separadores
+		System.out.println("a = " +a +"\nb = " +b + "\nc = " +c + "\nd = " +d);
+
+	}
+
+}
---
+++ CursoRevisado/src/ejercicio1/HolaMundo.java
@@ -0,0 +1,21 @@
+package ejercicio1;
+
+/**
+ * Ejercicio 1 de la presentación Programación Java
+ * @author paula.sanchez.tirado
+ *
+ */
+
+public class HolaMundo{
+	
+	/**
+	 * Imprime por pantalla Hola Mundo
+	 * @param args
+	 */
+
+	public static void main (String[] args){
+
+		System.out.println("Hola Mundo");
+
+	}
+}
